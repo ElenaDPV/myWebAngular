97f4a55638852239b06485fea6af5d729546fc8f
Elena <elena.ormaza.front.dp@gmail.com>
2020-10-03 21:54:14 +0200

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  nombre: string = 'Nombre Usuario';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  cambiarNombre(){
+    this.nombre='Elena menendez';
   }
 
 }
