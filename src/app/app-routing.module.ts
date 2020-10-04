64a002d65c84713115193c4f4e616ef15cd7b3ca
Elena <elena.ormaza.front.dp@gmail.com>
2020-10-04 11:55:31 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ConocimientosComponent } from './conocimientos/conocimientos.component';
+import { FreelanceComponent } from './freelance/freelance.component';
+import { TrayectoriaComponent } from './trayectoria/trayectoria.component';
+
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'conocimiento', component: ConocimientosComponent},
+  {path: 'trayectoria', component: FreelanceComponent},
+  {path: 'freelace', component: TrayectoriaComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
+    
     HomeComponent,
     ConocimientosComponent,
     TrayectoriaComponent,
